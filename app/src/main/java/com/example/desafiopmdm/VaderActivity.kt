1912d3a1fa195692d1a19d6cd8480a77d7b56684
David <molera.ps4@gmail.com>
2022-11-11 00:41:42 +0100

    Vader interfaz y comporbaciones conexion

---
+++ app/src/main/java/com/example/desafiopmdm/VaderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.desafiopmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vader)
+    }
+}
---
+++ app/src/main/java/factorias/FactoriaPersonas.kt
@@ -0,0 +1,4 @@
+package factorias
+
+object FactoriaPersonas {
+}
