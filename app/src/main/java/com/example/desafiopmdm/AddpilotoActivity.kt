e2f2cfb6db2da1630296e14442acc4f0adf9e248
David <molera.ps4@gmail.com>
2022-11-17 18:52:24 +0100

    Funcionalidades del piloto

---
+++ app/src/main/java/com/example/desafiopmdm/AddpilotoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.desafiopmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddpilotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addpiloto)
+    }
+}
